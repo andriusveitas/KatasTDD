f2453c92111abe1867c016dc0a14efc7e0027139
DESKTOP-V22J7SU\andri <andrius.veitas@mediapark.com>
2022-04-25 19:11:41 +0300

    StringCalculator project added. CalculatorCore implementaion started.

---
+++ Katas/Katas.StringCalculator.Tests/CalculatorCoreTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Katas.StringCalculator.Tests
+{
+    public class CalculatorCoreTests
+    {
+        private CalculatorCore _calculatorCore;
+        public CalculatorCoreTests()
+        {
+            _calculatorCore = new CalculatorCore();
+        }
+
+        [Fact]
+        public void AddingEmptyStringEqualsZero()
+        {
+            var init = string.Empty;
+            var actual = _calculatorCore.Add(init);
+            int expected = 0;
+            Assert.Equal(actual, expected);
+        }
+    }
+}
---
+++ Katas/Katas.StringCalculator/CalculatorCore.cs
@@ -0,0 +1,15 @@
+﻿namespace Katas.StringCalculator
+{
+    public class CalculatorCore
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            return -11110;
+        }
+    }
+}
